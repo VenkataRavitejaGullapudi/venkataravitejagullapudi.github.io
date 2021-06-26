0b11050b9171f8259ac7870c6530b143abb731c2
Raviteja <venkata.gullapudi@infor.com>
2021-06-26 14:01:15 +0530

    Update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {HashRouter, Route} from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import Home from '../Home/Home';
 import About from '../About/About';
 import Projects from '../Projects/Projects';
@@ -12,15 +12,15 @@ import { useState } from 'react';
 const App = () => {
     const [isOpenMenu, setIsOpenMenu] = useState(false)
     return (
-        <div>
+        <div className="main">
             <HashRouter>
-                <NavBar isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu}/>
-                <Route path='/' exact component={Home}/>
-                <Route path='/about' exact component={About}/>
-                <Route path='/projects' exact component={Projects}/>
-                <Route path='/skills' exact component={Skills}/>
-                <Route path='/contact' exact component={Contact}/>
-                <GoHome/>
+                <NavBar isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu} />
+                <Route path='/' exact component={Home} />
+                <Route path='/about' exact component={About} />
+                <Route path='/projects' exact component={Projects} />
+                <Route path='/skills' exact component={Skills} />
+                <Route path='/contact' exact component={Contact} />
+                <GoHome />
             </HashRouter>
         </div>
     )
